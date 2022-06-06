977d0d162ee8f1cf45dec112e797027264e7514a
Jose Alvarez <5135@holbertonschool.com>
2022-06-06 09:43:16 -0700

    Hello World

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
-		return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 5);
+		return (1);
 }
