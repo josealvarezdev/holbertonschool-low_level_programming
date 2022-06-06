45d9ffa8a9184c0548debd23d94ddda200dcb290
Jose Alvarez <5135@holbertonschool.com>
2022-06-06 07:03:08 -0700

    Hello World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
-int main (void){
+/* Print a strin|g  */
+int main(void)
+{
+	/* Use this */
 	char str1[] = ""Programming is like building a multilingual puzzle";
+
 	puts(str1);
 	return 0;
-
-
 }
