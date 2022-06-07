c73db4030b337b47eee1d8d8b8b061167ce1b07f
Jose Alvarez <5135@holbertonschool.com>
2022-06-07 06:19:32 -0700

    Variables C

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  *  * main - Prints a hexadecimal
+ *   *   *
+ *    *    * Return: Always
+*/
+int main(void)
+{
+int i, a,
+for (int i = 0; i < 9; i++)
+{
+for (int a = 1; a < 9; a++)
+{
+if (a != i)
+{
+putchar(i);
+putchar(a);
+}
+}
+}
+}
