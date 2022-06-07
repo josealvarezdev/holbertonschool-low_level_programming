aa01a1c342ea3556d2991accfa9883861a0e9344
Jose Alvarez <5135@holbertonschool.com>
2022-06-06 19:20:59 -0700

    Variables C

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  * main - Prints a hexadecimal
+ *   *
+ *    * Return: Always
+*/
+int main(void)
+{
+char a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+if (a != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+putchar(\n);
+return (0);
+}
