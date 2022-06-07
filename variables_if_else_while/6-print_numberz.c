c5179c8535c2dfa647b973ae4f52b37103dc990a
Jose Alvarez <5135@holbertonschool.com>
2022-06-06 18:41:33 -0700

    Variables C

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,15 @@
  *  *  *  *  * main - Entry point
  *   *   *   *   *
  *    *    *    *    * Return: Always 0 (Success)
- *    */
+*/
 
 int main(void)
 {
 	int c;
 	for (c = 0 ; c <= 9 ; ++c)
+	{
 	putchar(c);
 	putchar('\n');
+	}
 	return (0);
 }
