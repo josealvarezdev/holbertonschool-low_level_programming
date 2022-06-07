4d411e8c9381f8a67fe52161e72d9e52bfe021e6
Jose Alvarez <5135@holbertonschool.com>
2022-06-06 17:40:21 -0700

    Variables C

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int ld;
 ld = n % 10;
 if (n > 5)
 {
-	printf("Last digit of %d is %d and is greater than 5", n, ld);
+	printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 }
 else if (n == 0)
 {
-	printf("Last digit of %d is %d and is 0", n, ld);
+	printf("Last digit of %d is %d and is 0\n", n, ld);
 }
 else if (n < 6 AND n != 0)
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0");
+	printf("Last digit of %d is %d and is less than 6 and not 0\n");
 }
 return (0);
 }
