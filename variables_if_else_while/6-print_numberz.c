522df6ebf4c7eeda86c20b110c50b748295e4ca5
Jose Alvarez <5135@holbertonschool.com>
2022-06-06 18:47:17 -0700

    Variables C

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,11 @@
 
 int main(void)
 {
-	int c;
-	for (c = 0 ; c <= 9 ; ++c)
-	{
-	putchar(c);
-	}
-	return (0);
+int c;
+for (c = '0' ; c <= '9' ; ++c)
+{
+putchar(c);
+putchar('\n');
+}
+return (0);
 }
