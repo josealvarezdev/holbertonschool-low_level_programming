5a24c8afa9ed70e23fa2adc2a9c3f05b4589399e
Jose Alvarez <5135@holbertonschool.com>
2022-06-07 15:52:37 -0700

    Variables C

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -6,24 +6,31 @@
 */
 int main(void)
 {
-int num1, num2;
+int num1, num2, num3, num4;
 
-for (num1 = 0; num1 <= 99; num1++)
+for (num1 = '0'; num1 <= '9'; num1++)
 {
-for (num2 = 0; num1 <= 99; num2++)
+for (num2 = '0'; num2 <= '9'; num2++)
 {
-if ( num1 < num2 && num1 != num2 )
+for (num3 = '0'; num3 <= '9'; num3++)
 {
-putchar(num1/10);
-putchar(num1%10);
+for (num4 = '1'; num4 <= '9'; num4++)
+{
+
+	if ( num1 < num2 && >w )
+{
+putchar(num1);
+putchar(num2);
 putchar(' ');
-putchar(num2/10);
-putchar(num2%10);
+putchar(num3);
+putchar(num4);
 
 if (num1 != 98 || num2 != 99)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 }
