0457fc71c50abf4dd5653fd52c7db5e55bce49a3
Jose Alvarez <5135@holbertonschool.com>
2022-06-08 16:20:50 -0700

    Functions

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
- *  * main - Prints a string with putchars
- *   *
- *    * Return: Always 0
- *     */
+ * * main -  _putchar
+ * * Return: Always 0
+ * */
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return(0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
