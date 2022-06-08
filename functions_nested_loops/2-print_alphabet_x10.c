1b02b64d8cc23d4bbb3d586acbe5dfc7488cca1f
Jose Alvarez <5135@holbertonschool.com>
2022-06-08 16:57:42 -0700

    Functions

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  * main - check the code.
+ *   *
+ *    * Return: Always 0.
+*/
+int main(void)
+{
+	int count
+	char alphabet
+	void print_alphabet(void)
+	{
+		for (count = 0; count <= 10; count++)
+		{
+			for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			{
+				_putchar(alphabet);
+			}
+		}
+
+
+	}
+	_putchar(10);
+	return (0);
+}
