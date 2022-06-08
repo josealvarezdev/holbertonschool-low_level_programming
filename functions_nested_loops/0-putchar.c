a62dd58f97d130bc032f5cfdd7c6149c2a9b185a
Jose Alvarez <5135@holbertonschool.com>
2022-06-08 16:07:32 -0700

    Functions

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,12 +9,12 @@ int main(void)
  * This program print _putchar with a library _putchar.h
 */
 {
-_putchar (_);
-_putchar (p);
-_putchar (u);
-_putchar (t);
-_putchar (c);
-_putchar (a);
-_putchar (r);
+_putchar ('_');
+_putchar ('p');
+_putchar ('u');
+_putchar ('t');
+_putchar ('c');
+_putchar ('a');
+_putchar ('r');
 return (0);
 }
