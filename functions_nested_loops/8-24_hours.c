d1bbaca8bfd73814afacf1c892979142195ea4f1
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 09:16:52 -0700

    Functions

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 int num1, num2;
 for (num1 = 0; num1 <= 23; num1++)
 {
-	for (num2 = num1; num2 <= 60; num2++)
+	for (num2 = num1; num2 < 60; num2++)
 	{
 		_putchar((num1 / 10) + '0');
 		_putchar((num1 % 10) + '0');
