701859c52d852a7b16654c9240c746e21d5d4432
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 06:19:00 -0700

    Functions

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ *  *  * print_sign - write a function that says if an int is positive, negative or 0
+ *   *   * @c: - is a variable dou
+ *    *    * Return: 1 if c is a letter, lowercase or uppercase
+ *    */
 int print_sign(int n)
 {
 	if (n > 0)
