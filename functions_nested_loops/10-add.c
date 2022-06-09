b0413250200155621c4350ee516d1f784793c6a2
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 10:18:25 -0700

    Functions

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
 * add - suma de dos variables int
-* @resultado: - is a variable
+* @a: - is a variable
+* @b: is a variable 
 * Return: resultado de a+b
 */
 int add(int a, int b)
@@ -10,5 +11,5 @@ int resultado;
 
 resultado = a + b;
 
-return(resultado);
+return (resultado);
 }
