bd76abf3d91521def51a3e55c6e48a847df34ddb
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 06:43:15 -0700

    Functions

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  *  * _abs - return absolute value of a number
+ *   *   * @c: - is a variable dou
+ *    *    * Return: 1 if c is a letter, lowercase or uppercase
+*/
+int _abs(int r);
+
+if (r>=0)
+{
+return (r);
+}
+else
+{
+return (-r);
+}
