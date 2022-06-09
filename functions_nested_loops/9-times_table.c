af2e2e3dd10687ee667fafcabe6c689c5745df69
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 09:51:41 -0700

    Functions

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  *  * times_table - print 9 times table starting with zero
+ *    *    * Return: 0
+*/
+void times_table(void)
+{
+int col, fila, num;
+
+num = 0;
+for (col = 0; col <= 9; col++)
+{
+for (fila = 0; fila <= 9; fila++)
+{
+num = col*fila;
+_putchar(num);
+}
+}
+}
