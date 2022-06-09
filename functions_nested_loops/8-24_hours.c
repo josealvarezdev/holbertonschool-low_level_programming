6e2f6489fac7d64e449cfbf5b3f649de5af1536e
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 07:32:10 -0700

    Functions

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,19 +11,19 @@ for (num1 = 0; num1 <= 23; num1++)
 {
 	for (num2 = num1; num2 <= 59; num2++)
 	{
-		putchar((num1 / 10) + '0');
-		putchar((num1 % 10) + '0');
-		putchar(' ');
-		putchar((num2 / 10) + '0');
-		putchar((num2 % 10) + '0');
-		if (num1 != 98 || num2 != 99)
+		_putchar((num1 / 10) + '0');
+		_putchar((num1 % 10) + '0');
+		_putchar(':');
+		_putchar((num2 / 10) + '0');
+		_putchar((num2 % 10) + '0');
+		/*if (num1 != 23 || num2 != 59)
 		{
-			putchar(',');
-			putchar(' ');
-		}
+			_putchar(',');
+			_putchar(' ');
+		} */
 	}
 }
-putchar('\n');
+_putchar('\n');
 return (0);
 }i
 }
