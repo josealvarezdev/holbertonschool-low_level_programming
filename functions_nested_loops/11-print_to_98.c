58eb3047407ad9005f9fe2bc9bad1b5856959439
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 10:29:04 -0700

    Functions

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 void print_to_98(int n)
 {
 for (n = 0; n < 99; n++)
 {
-	_putchar(n);
+	printf(%d, n);
 	if(n != 98)
-		_putchar(',');
-		_putchar(' ');
+		printf(", ");
 }
 }
