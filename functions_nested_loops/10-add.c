26b66fa95b80be342b71c71f9ecef70da787117c
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 10:15:41 -0700

    Functions

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+//Comentame
+int add(int a, int b)
+{
+int resultado;
+
+resultado = a + b;
+
+return(resultado);
+}
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,7 +18,9 @@ if (num != fila * 9)
 {
 _putchar (',');
 _putchar (' ');
-_putchar (10);
+/*
+ * _putchar (10);
+*/
 }
 }
 }
