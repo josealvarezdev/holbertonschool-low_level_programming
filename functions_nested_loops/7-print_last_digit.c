3c4aaad858731b532457a85069b312e125b84d55
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 06:51:13 -0700

    Functions

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  *  * _abs - return absolute value of a number
+ *   *   * @r: - is a variable dou
+ *    *    * Return: 1 if c is a letter, lowercase or uppercase
+*/
+int print_last_digit(int r);
+{
+_putchar(r % 10)
+return(r);
+}
