9a83ec2261c742f4accee0ffa9d93d9f1321e28f
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 05:48:41 -0700

    Functions

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,15 +2,16 @@
 
 /**
  *  * _isalpha - write a function that checks for alphabetic character
- *   * c - is a variable dou
+ *   * @c: - is a variable dou
  *    * Return: 1 if c is a letter, lowercase or uppercase
 */
 int _isalpha(int c)
 {
-c = 0;
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
-return (0);
+else {
+	return (0);
+}
 }
