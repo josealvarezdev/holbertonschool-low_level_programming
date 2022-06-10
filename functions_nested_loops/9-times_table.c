c21efd04380744457d264b4dfa334187375c4bfd
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 18:34:19 -0700

    Functions

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,19 +8,17 @@ void times_table(void)
 int col, fila, num;
 
 num = 0;
-for (fila = 0; fila <= 9; fila++)
-{
-for (col = 0; col <= 9; col++)
-{
-num = col * fila;
-_putchar (num + '0');
-if (num != fila * 9)
-{
-_putchar (',');
-_putchar (' ');
-/*
- * _putchar (10);
-*/
+	for (fila = 0; fila <= 9; fila++)
+	{
+		_putchar(num);
+		for (col = 0; col <= 9; col++)
+		{
+			num = col * fila;
+			_putchar (num + '0');
+		if (num != fila * 9)
+		{
+			_putchar (',');
+			_putchar (' ');
 }
 }
 }
