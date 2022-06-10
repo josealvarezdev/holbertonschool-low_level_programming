739d96bf6c8f07a2f023eba0e3213968dc18e4f5
Jose Alvarez <5135@holbertonschool.com>
2022-06-09 17:22:06 -0700

    Functions

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,7 @@
 #include <stdio.h>
 
 void print_to_98(int n)
-{	
-/*	if (n <= 98 && n != 99)
-	{
-	printf("%d", n);
-	n++;*/
+{
 	while (n >= 98)
 	{
 		if(n != 98)
@@ -17,7 +13,7 @@ void print_to_98(int n)
 		else if (n == 98)
 		{
 		printf("%d", n);
-		}
+			}
 	}
 printf("\n");
 }
--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,12 @@
 int print_last_digit(int r)
 {
 	int last;
-last = r % 10;
-if (last < 0)
-{
-last = -last;
-}
-_putchar (last + '0');
-return (last);
+	
+	last = r % 10;
+	if (last < 0)
+	{
+		last = -last;
+	}
+	_putchar (last + '0');
+	return (last);
 }
