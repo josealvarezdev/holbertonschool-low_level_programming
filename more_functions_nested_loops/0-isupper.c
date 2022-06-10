0844f085eddabe8a32136f71ca85637afd178d2e
Jose Alvarez <5135@holbertonschool.com>
2022-06-10 06:48:40 -0700

    More nested loops

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
+/*
+ * _isupper
+ * Return: 1 if letter is upper and 0 ifnot
+ * @c: Is a input
+*/
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 {
 return (1);
 }
-else 
+else
 {
 return (0);
 }
