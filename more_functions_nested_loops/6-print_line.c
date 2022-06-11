5c2ecbf544c8b83dd585846df4b25c38bb47ab51
Jose Alvarez <5135@holbertonschool.com>
2022-06-11 15:09:12 -0700

    More nested loops

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * print_line - draws a line in the terminal
+ * @n: print lines
+ *
+*/
+
+void print_line(int n)
+{
+int a
+
+if (n > 0)
+{
+for (a = 0; a < n; a++)	
+{
+	_putchar('_');
+}
+_putchar(10);
+}
+}
+
