24dbcfc18bd362fe5008c15a8c2b26471eb9c54d
Jose Alvarez <5135@holbertonschool.com>
2022-06-11 15:23:52 -0700

    More nested loops

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,17 @@ int a, b;
 if (n > 0)
 {
 	for(b = 0; b < n; b++)
-	{
+{
 		for (a = 0; a < n; a++)
 		{
 			_putchar(' ');
 			_putchar(92);
 			_putchar(10);
 		}
-	}	
+}	
 else
 {
-	_putchar(10);
+_putchar(10);
 }
 }
 }
