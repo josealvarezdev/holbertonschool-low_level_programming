c9e120642a554e9904e52a72894ee66c37b210a3
Jose Alvarez <5135@holbertonschool.com>
2022-06-12 07:28:25 -0700

    More nested loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/**
+ * int main - function of print fizzbuzz test
+*/
 int main(void)
 {
 char count;
@@ -22,10 +24,10 @@ for (count = 1; count < 100; count++)
 	{
 		printf("%d", count);
 	}
-	if (count <= 99)
-	{
-		putchar(' ');
-	}
+if (count <= 99)
+{
+putchar(' ');
+}
 }
 printf ("Buzz\n");
 return (0);
