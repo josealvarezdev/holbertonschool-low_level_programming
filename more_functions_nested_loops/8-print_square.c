5e2bcc7d336ae0fb315a46074bf5ee3961c25113
Jose Alvarez <5135@holbertonschool.com>
2022-06-12 06:58:29 -0700

    More nested loops

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,12 @@
 #include "main.h"
-
+/**
+ * print_square - print squares with #
+ * @size: is how long print squares
+ *
 void print_square(int size)
 {
 
-int a,b;
+int a, b;
 
 if (size <= 0)
 {
@@ -11,9 +14,9 @@ if (size <= 0)
 }
 else
 {
-for(a = 0; a < size; a++)
+for (a = 0; a < size; a++)
 {
-for(b = 0; b < size; b++)
+for (b = 0; b < size; b++)
 {
 _putchar(35);
 }
