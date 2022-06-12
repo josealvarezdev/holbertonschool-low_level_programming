489ac83f1772f9176eba873dab14052d1406888a
Jose Alvarez <5135@holbertonschool.com>
2022-06-12 08:13:43 -0700

    Debugging

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -11,6 +11,12 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 i = 0;
+/**
+ * * while (i < 10) - description 
+ * * {
+ * * putchar(i);
+ * * }
+*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
