9f999fa5146ba805d2d3313f6b9406c3a406aba2
Jose Alvarez <5135@holbertonschool.com>
2022-06-12 07:03:54 -0700

    More nested loops

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+int count;
+
+for (count = 0; count <= 100' count++)
+if (count % 15)
+	printf("FizzBuzz");
+else if (count % 3)
+	printf("Fizz");
+else if (count % 5)
+	printf("Buzz");
+}
