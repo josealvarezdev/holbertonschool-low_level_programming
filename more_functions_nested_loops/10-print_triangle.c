7215a4ca96ebae3196b3f261b54e872d45909222
Jose Alvarez <5135@holbertonschool.com>
2022-06-12 07:51:47 -0700

    More nested loops

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,19 +5,18 @@
 */
 void print_triangle(int size)
 {
-	int a,b,c;
+	int a, b, c;
 
 if (size <= 0)
 	_putchar('\n');
 for (a = 0; a < size; a++)
 {
-	for(b = size - a; b > 1; b--)
+	for (b = size - a; b > 1; b--)
 		_putchar(' ');
-	for(c = a + b; c >= 1; c--)
+	for (c = a + b; c >= 1; c--)
 	{
 		_putchar(35);
 	}
 _putchar(10);
 }
 }
-	
