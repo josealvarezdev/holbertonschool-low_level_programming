0e7f91fe288721e82e92e4252be8d01176de8655
Jose Alvarez <5135@holbertonschool.com>
2022-06-12 06:50:12 -0700

    More nested loops

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,20 +4,19 @@ void print_diagonal(int n)
 {
 int a, b;
 
-if (n > 0)
+if (n <= 0)
 {
-for(b = 0; b < n; b++)
-{
-for (a = 0; a < n; a++)
-{
-	_putchar(' ');
-}
-_putchar(92);
-_putchar(10);
-}
+_putchar('\n');
 }
 else
 {
-_putchar(10);
+for (a = 0 ; a < n ; a++)
+{
+for (b = 0 ; b < a ; b++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');														}
 }
 }
