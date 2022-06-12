40ceda96c31c41ad0a4baa4d4a6c1de999b06bc2
Jose Alvarez <5135@holbertonschool.com>
2022-06-12 08:11:12 -0700

    Debugging

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -3,16 +3,16 @@
 /**
  * * main - causes an infinite loop
  * * Return: 0
- * */
+*/
 
 int main(void)
 {
-	        int i;
+int i;
 
 printf("Infinite loop incoming :(\n");
 i = 0;
 /**
- * while (i < 10)
+ * while (i < 10) - this dosent work well
  * {
  * putchar(i);
  * }
