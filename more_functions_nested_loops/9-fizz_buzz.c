05874aa9542df9435d7a0bfd27a492941ba32f76
Jose Alvarez <5135@holbertonschool.com>
2022-06-12 07:14:43 -0700

    More nested loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,15 +6,19 @@ int count;
 
 for (count = 0; count <= 100; count++)
 if ((count % 5 == 0 ) && (count % 3 == 0))
-	printf("FizzBuzz ");
-else if ((count % 3) == 0)
-	printf("Fizz ");
-else if ((count % 5) == 0)
-	printf("Buzz ");
+	printf("FizzBuzz");
+else if (count % 3 == 0)
+	printf("Fizz");
+else if (count % 5 == 0)
+	printf("Buzz");
 else
 {
 	printf("%d", count);
 }
-printf("\n");
+if (count < 100)
+{
+putchar (' ');
+}
+printf('\n');
 return(0);
 }
