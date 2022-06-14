8396f2e0075d3aa51f7c8d76734fb0c8c5bb683b
Jose Alvarez <5135@holbertonschool.com>
2022-06-14 10:51:31 -0700

    Pointers and arrays

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - print array
+ * @str: array char
+ * Return: Void
+*/
+void print_rev(char *s)
+{
+int count = 0;
+
+count = 0;
+while (*s != '\0')
+{
+count++;
+s++;
+}
+for(; count == 0; count--)
+{
+_putchar(s[count]);
+}
+}
