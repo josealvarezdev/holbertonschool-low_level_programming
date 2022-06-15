f2014eb67ecb00b3f888a3bc82a370e5327555d2
Jose Alvarez <5135@holbertonschool.com>
2022-06-15 10:02:25 -0700

    Pointers and arrays

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,10 +3,9 @@ void puts2(char *str)
 {
 	int count = 0;
 
-	while (str[count] != '\0')
+	while ((str[count] != '\0') && ((count % 2) == 0))
 	{
 	_putchar(str[count]);
-	count = count +2;
 	}
 _putchar(10);
 }
