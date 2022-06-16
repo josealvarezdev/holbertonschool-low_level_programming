5285b8065cd7b94cca842c4014007df4660162f3
Jose Alvarez <5135@holbertonschool.com>
2022-06-16 09:33:23 -0700

    Pointers and arrays

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncat - fun that concatenates two strings
+ * @dest: destino
+ * @src: original
+ * n: var
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+size_t dest_len = strlen(dest);
+size_t i;
+
+for (i = 0 ; i < n && src[i] != '\0' ; i++)
+dest[dest_len + i] = src[i];
+dest[dest_len + i] = '\0';
+return dest;
+}
