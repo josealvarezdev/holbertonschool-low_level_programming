50b6185aea0b7668d506fa245ba4312877bf533e
Jose Alvarez <5135@holbertonschool.com>
2022-06-16 09:14:39 -0700

    Pointers and arrays

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 char *_strcat(char *dest, char *src)
 {
 if (!src || !dest)
@@ -11,6 +11,7 @@ size_t dest_len = strlen(dest);
 size_t i;
 
 	for (i = 0 ; src[i] != '\0' ; i++)
+
 	dest[dest_len + i] = src[i];
 	dest[dest_len + i] = '\0';
 	return (dest);
