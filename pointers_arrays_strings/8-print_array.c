4d283b0d56cac94f5c4daffc8310d252429792c6
Jose Alvarez <5135@holbertonschool.com>
2022-06-16 05:36:02 -0700

    Pointers and arrays

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - print n elements of an array of integers
+ * @a: var 1
+ * @n: var 2
+ * Return: Nothing
+*/
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	while (a[count] != '\0' || count <= n)
+	{
+		_putchar(str[count]);
+		count++;
+	}
+	_putchar(10);
+}
