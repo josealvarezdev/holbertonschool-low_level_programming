67ab131262577bda6e75e0d402b7478317a7e7e3
Jose Alvarez <5135@holbertonschool.com>
2022-06-16 09:13:44 -0700

    Pointers and arrays

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,7 @@ return (0);
 size_t dest_len = strlen(dest);
 size_t i;
 
-	for (i = 0 ; src[i] != '\0' ; i++)|:
+	for (i = 0 ; src[i] != '\0' ; i++)
 	dest[dest_len + i] = src[i];
 	dest[dest_len + i] = '\0';
 	return (dest);
