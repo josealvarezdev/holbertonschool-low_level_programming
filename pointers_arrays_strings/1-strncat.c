1711cf903549775943f2bc676b23e21c6845af81
Jose Alvarez <5135@holbertonschool.com>
2022-06-16 09:42:38 -0700

    Pointers and arrays

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 size_t dest_len = strlen(dest);
 size_t i;
 
-for (i = 0 ; i < n && src[i] != '\0' ; i++)
+for (i = 0 ; src[i] != '\0' ; i++)
 dest[dest_len + i] = src[i];
 dest[dest_len + i] = '\0';
 return dest;
