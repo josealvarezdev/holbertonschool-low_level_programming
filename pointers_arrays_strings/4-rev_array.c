682784c04a768fe366335bee0d4613341254e175
Jose Alvarez <5135@holbertonschool.com>
2022-06-16 10:41:22 -0700

    Pointers and arrays

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -3,15 +3,10 @@
 
 void reverse_array(int *a, int n)
 {
-	int len = 0;
 	int i;
 	char temp;
 
-	while (a[len] != '\0')
-	{
-		len++;
-	}
-	for (i = 0; i < (len / 2); i++)
+	for (i = 0; i < (n / 2); i++)
 	{
 		temp = a[i];
 		a[i] = a[len - i - 1];
