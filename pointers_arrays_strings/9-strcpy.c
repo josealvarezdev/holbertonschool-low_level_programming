a2633a9ded5edd564f18a29e6e85cd8ceecc1c8a
Jose Alvarez <5135@holbertonschool.com>
2022-06-16 09:01:27 -0700

    Pointers and arrays

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _strcpy - copy array
+ * @dest: destino de copia
+ * @src: original
+ * Return: 0;
 char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
 if (!src || !dest)
 {
-	return(0);
+	return (0);
 }
 while (src[count] != '\0')
 {
@@ -14,5 +18,5 @@ dest[count] = src[count];
 count++;
 }
 dest[count] = '\0';
-return(dest);
+return (dest);
 }
