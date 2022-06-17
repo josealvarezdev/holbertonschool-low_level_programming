c0e1295f130164b887001c564579cdafda62ab99
Jose Alvarez <5135@holbertonschool.com>
2022-06-16 18:25:33 -0700

    Pointers and arrays

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,15 @@
 #include "main.h"
 #include <string.h>
 
-
+char *string_toupper(char *)
+{
+int i = 0;
+while (*[i] != '\0')
+{
+if (*[i] >= 'a' && *[i] <= 'z')
+{
+*[i] = *[i] - 32;
+}
+i++;
+}
+}
