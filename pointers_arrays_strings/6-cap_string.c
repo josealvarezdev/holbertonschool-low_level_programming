016fa4194136531da78586e1686cd5bc553a1be2
Jose Alvarez <5135@holbertonschool.com>
2022-06-17 09:45:22 -0700

    Pointers and arrays

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+char *cap_string(char *)
+{
+int b[13] = {' ', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int i = 0;
+int j = 0;
+
+for (; a[i] != '\0'; i++)
+{
+	for(; b[j] != '\0'; j++)
+	{
+		if (b[j] == a[i])
+		{
+			a[i + 1] = a[i + 1] - 32;
+		}
+	}
+}
+return (a);
+}
