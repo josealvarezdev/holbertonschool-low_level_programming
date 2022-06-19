28dc487af252c8481eeb188fad52beda86fb8e65
Jose Alvarez <5135@holbertonschool.com>
2022-06-19 16:13:44 -0700

    Pointers and arrays

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_diagsums - print the sum of two diagonals
+ * @a: array
+ * @size: size of array
+ * Return: void
+*/
 void print_diagsums(int *a, int size)
 {
 int sum1, sum2, i;
