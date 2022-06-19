27c3b2e66154ed0ae60dba2189f5b06112e844af
Jose Alvarez <5135@holbertonschool.com>
2022-06-19 08:05:12 -0700

    Pointers and arrays

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,15 +4,15 @@ char *leet(char *a)
 {
 int i,j;
 char b[10] = "AaEeOoTtLl";
-char translate[10] = "4433007711";
+char bt[10] = "4433007711";
 
 for (i = 0; a[i] != '\0'; i++)
 {
 	for (j = 0; j <= 10; j++)
 	{
-		if (a[i] == translate[j])
+		if (a[i] == b[j])
 		{
-			a[i] == translate[j];
+			a[i] == bt[j];
 		}
 	}
 return (a);
