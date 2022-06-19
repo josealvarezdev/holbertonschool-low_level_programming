9aa10ef88bf8738b88b55045707a6bb26b5595a5
Jose Alvarez <5135@holbertonschool.com>
2022-06-19 09:59:41 -0700

    Pointers and arrays

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,4 +21,5 @@ char *cap_string(char *a);
 char *leet(char *a);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
