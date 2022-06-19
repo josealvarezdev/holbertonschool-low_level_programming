267ff12b22556b31180376e82b934ebf3bcc5009
Jose Alvarez <5135@holbertonschool.com>
2022-06-19 11:12:28 -0700

    Pointers and arrays

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strpbrk - search a string
+ * @s: array original
+ * @accept: string to compare with ori
+ * Return: pointer to byte in s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+	if (!accept[j])
+	{
+	break;
+	}
+	}
+return (*s[i]);
+}
