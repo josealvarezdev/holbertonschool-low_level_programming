3ec2ae1348cb02c5276d823874a9f7e081cfb7a3
Jose Alvarez <5135@holbertonschool.com>
2022-06-19 10:38:06 -0700

    Pointers and arrays

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpn - Get the lengh of a prefix substring
+ * @s: array base
+ * @accept: substring
+ * Return: number of bytes in the initial segment of s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+			return (i);
+			}
+		}
+	}
+}
