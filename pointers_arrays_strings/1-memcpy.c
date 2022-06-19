f3c573c91bc1a11d277f295f1c194910407368d1
Jose Alvarez <5135@holbertonschool.com>
2022-06-19 09:52:25 -0700

    Pointers and arrays

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: array destino
+ * @src: array de origen
+ * @n: copy n bytes from memory of src to memory area dest
+ * Return: pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+return(dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,4 +20,5 @@ char *cap_string(char *a);
 char *cap_string(char *a);
 char *leet(char *a);
 char *_memset(char *s, char b, unsigned int n);
+har *_memcpy(char *dest, char *src, unsigned int n);
 #endif
