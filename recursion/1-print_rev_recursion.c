ff7eb6ae1609eebf725e5dc10cc2c177afffa29f
Jose Alvarez <5135@holbertonschool.com>
2022-06-20 07:23:42 -0700

    Pointers and arrays

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _print_rev_recursion
+ * @s: array
+ * Return: void
+*/
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
@@ -8,7 +12,7 @@ void _print_rev_recursion(char *s)
 	}
 	else
 	{
-	_print_rev_recursion (s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar (*s);
 	s++;
 	}
