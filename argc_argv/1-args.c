4d3b99f5f7542c6f4a546a48f7a390387089568f
Jose Alvarez <5135@holbertonschool.com>
2022-06-21 10:39:57 -0700

    Argc and Argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print number of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+*/
+int main (int argc, char *argv[])
+{
+printf("%d", argc);
+}
