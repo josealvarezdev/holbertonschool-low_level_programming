30f4cab5f96fce8ca21e797eff45402ad8e0b4bb
Jose Alvarez <5135@holbertonschool.com>
2022-06-21 11:06:38 -0700

    Argc and Argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print res of multiplication of arguments
+ * @argc: size of array
+ * @argv: char array
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int res = argv[1] * argv[2];
+printf("%d\n", res);
+return (0);
+}
