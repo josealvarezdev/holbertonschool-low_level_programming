81710abd62689a02298f40be4466d8f8c5f38530
Jose Alvarez <5135@holbertonschool.com>
2022-06-21 13:30:35 -0700

    Argc and Argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - add digits
+ * @argc: num
+ * @argv: array
+ * Return: numeritos
+*/
 int main(int argc, char *argv[])
 {
 int i, j, add;
@@ -11,9 +17,9 @@ add = 0;
 	}
 	for (j = 1; j < argc; j++)
 	{
-		for (i = 0; argv[i]; i++)
+		for (i = 0; argvi[j][i]; i++)
 		{
-			if ((*argv[i] >= 'a' && *argv[i] <= 'z') || (*argv[i] >= 'A' && *argv[i] <= 'Z'))
+			if ((*argv[j][i] >= 'a' && *argv[j][i] <= 'z') || (*argv[j][i] >= 'A' && *argv[j][i] <= 'Z'))
 			{
 				printf("Error\n");
 				return (1);
