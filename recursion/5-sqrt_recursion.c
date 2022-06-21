1e34b5ce7b17571b292cbefc088e8cbd09a7b2ee
Jose Alvarez <5135@holbertonschool.com>
2022-06-21 05:50:38 -0700

    Pointers and arrays

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,14 +1,7 @@
 #include "main.h"
-
-int _sqrt_recursion(int n)
-{
-return (_sqrtm(n, 0);
-}
-
 /**
  * Extra function to resolve sqrt
 */
-
 int _sqrtm(int n, int num2)
 {
 int raiz = num2 * num2;
@@ -21,3 +14,9 @@ if (raiz > n)
 return (-1);
 }
 return (_sqrtm(n, num +1));
+
+/*Algo*/
+int _sqrt_recursion(int n)
+{
+return (_sqrtm(n, 0);
+}
