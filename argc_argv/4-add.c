d73d9dbf11e2aad1b7bcdd1bab2118c38851f83c
Jose Alvarez <5135@holbertonschool.com>
2022-06-21 13:27:36 -0700

    Argc and Argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,14 +10,16 @@ add = 0;
 		return (0);
 	}
 	for (j = 1; j < argc; j++)
-	for (i = 0; argv[i]; i++)
 	{
-		if ((*argv[i] >= 'a' && *argv[i] <= 'z') || (*argv[i] >= 'A' && *argv[i] <= 'Z'))
+		for (i = 0; argv[i]; i++)
 		{
-			printf("Error\n");
-			return (1);
-		}
+			if ((*argv[i] >= 'a' && *argv[i] <= 'z') || (*argv[i] >= 'A' && *argv[i] <= 'Z'))
+			{
+				printf("Error\n");
+				return (1);
+			}
 add += atoi(argv[i]);
+		}
 	}
 printf("%d\n", add);
 return (0);
