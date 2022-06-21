11aef4cad976360b7b4373f999d045ff6c1f984a
Jose Alvarez <5135@holbertonschool.com>
2022-06-21 12:12:08 -0700

    Argc and Argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 int i, j, add;
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; j < argv[i]; j++)
+		for (j = 0; j < *argv[i]; j++)
 		{
 			add += atoi(argv[i]);
 		}
