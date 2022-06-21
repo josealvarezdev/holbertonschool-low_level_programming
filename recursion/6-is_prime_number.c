e7dc95aa789f0430b831375ea8eb60202e73fafb
Jose Alvarez <5135@holbertonschool.com>
2022-06-21 07:16:49 -0700

    Pointers and arrays

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
-int is_prime_number(int n)
+int prime(int i, int j)
 {
-	if (n > 0)
-	{
-	if (n = (n * 1))
-	{
-		return (1);
-	}
-	}
-	if (n == 0)
+	if (i == 1)
 	{
 		return (0);
 	}
-return (0);
+	if ((j % i) == 0)
+	{
+		return (0);
+	}
+return prime(i, j - 1);
+}
+int is_prime_number(int n)
+{
+return prime(i, j + 1);
 }
