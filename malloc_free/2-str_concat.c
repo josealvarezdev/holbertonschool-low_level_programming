7e02a2890d3865f5369ceef0b890c8ef14086146
Jose Alvarez <5135@holbertonschool.com>
2022-06-23 14:21:19 -0700

    Malloc is love

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,8 +10,21 @@ int lens1, lens2;
 int suma;
 int k;
 k = 0;
+if(s1 == NULL)
+{
+lens1 = 0;
+else
+{
 lens1 = strlen(s1);
+}
+if (s2 == NULL)
+{
+lens2 = 0;
+}
+else
+{
 lens2 = strlen(s2);
+}
 suma = lens1 + lens2;
 
 pos = malloc(lens1 + lens2 * sizeof(char) + 1);
