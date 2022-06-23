fbf67a92f5bb3b4d76c0700b2dae6fc234aa1eca
Jose Alvarez <5135@holbertonschool.com>
2022-06-23 15:57:29 -0700

    Malloc is love

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+int **alloc_grid(int width, int height)
+{
+int *fil, *coco;
+int i, j;
+if (width < 1 || height < 1)
+{
+return (NULL);
+}
+fil = malloc(width * sizeof(int));
+coco = (height * sizeof(int));
+
+if (fil == NULL)
+return (NULL);
+if (coco == NULL)
+return (NULL);
+
+for (i = 0; i < width; i++)
+{
+	coco[i] = 0;
+	for(j = 0; j < height; j++)
+	{
+		fil[j] = 0;
+	}
+}
+}
+
