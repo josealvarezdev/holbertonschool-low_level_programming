b5d8fd0935053088b2d779aa0bee320eb6b2d9b0
Jose Alvarez <5135@holbertonschool.com>
2022-06-23 10:34:37 -0700

    Malloc is love

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *str_concat(char *s1, char *s2)
+{
+char *pos;
+int a;
+int lens1, lens2;
+
+lens1 = strlen(s1);
+lens2 = strlen(s2);
+
+pos = malloc(lens1 + lens2 * sizeof(char) + 1);
+
+	for (a = 0; a < lens1; a++)
+	{
+		pos[a] = s1[a];
+	}
+	for (a = 0; a < lens2; a++)
+	{
+		pos[a] = s2[a];
+	}
+p[a + 1] = '\0';
+return (pos);
+}
