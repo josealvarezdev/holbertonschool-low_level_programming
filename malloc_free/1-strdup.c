8cf853e206f7fac6a88d9ebfd76800f25e231b6e
Jose Alvarez <5135@holbertonschool.com>
2022-06-22 19:16:29 -0700

    Malloc is love

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *_strdup(char *str)
+{
+int size;
+char *copy
+char *p;
+
+/*Allocate Memory for copy*/
+	size = strlen(str);
+	*copy = (char *)malloc(sizeof(char)*size+1);
+	if (copy == NULL)
+	{
+		return ((char *)NULL);
+	}
+/* Copy String*/
+	while (*str)
+	{
+		*p = *str;
+		*p++;
+		*str++;
+	}
+*p = '\0';
+
+return (copy);
+}
