fc1022909d97bf399284c54a0520e9d397612c87
Jose Alvarez <5135@holbertonschool.com>
2022-06-23 18:41:51 -0700

    Malloc is love

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -23,9 +23,9 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		m[i] = malloc(sizeof(int)* width);
-		if (m[i] = NULL)
+		if (m[i] == NULL)
 		{
-			for(i; i >= 0; i--)
+			for(; i >= 0; i--)
 			{
 				free(m[i]);
 			}
