15f7c83fa53b65cdfcf78dc2a9af60fb7489c9ac
Jose Alvarez <5135@holbertonschool.com>
2022-06-24 07:28:04 -0700

    Pointers and arrays

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,15 +14,26 @@ int *p;
 unsigned int sizes1;
 unsigned int i, j;
 
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
 sizes1 = strlen(s1);
 p = malloc((sizes1 + n) + 1);
+	if (p == NULL)
+	{
+	return (NULL);
+	}
 	for (i = 0; i <= sizes1; i++)
 	{
 		p[i] = s1[i];
 	}
 	for (j = 0; j <= n; j++)
 	{
-		p[i] = s[j];
+		p[i] = s22[j];
 		i++;	
-}
+	}
+p [i] = '\0';
+return (p);
 }
