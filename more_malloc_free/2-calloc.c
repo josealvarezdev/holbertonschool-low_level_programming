59f68cec7a8c31a4479a5aa9fb977696d097afa7
Jose Alvarez <5135@holbertonschool.com>
2022-06-24 08:50:22 -0700

    Pointers and arrays

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,11 +10,10 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *p;
-
 	
 	if (nmemb == 0 || size == 0)
 		return(NULL);
-	p = malloc(size * nmemb);
+	p = malloc (size * nmemb);
 	if (p == NULL)
 	{
 		return (NULL);
