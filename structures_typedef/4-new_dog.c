6bccc38482fb87bbf1582f4a416e9d1769b066db
Jose Alvarez <5135@holbertonschool.com>
2022-06-27 18:43:03 -0700

    Structures and typedef

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - dogchow
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: return
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *pos;
+int lname, lowner;
+
+pos = malloc(sizeof(dog_t));
+	if (pos == NULL)
+	{
+		return (NULL);
+	}
+lname = strlen(name);
+lowner = strlen(owner);
+
+pos->name = malloc(lname * sizeof(pos->name));
+	if (pos->name == NULL)
+	{
+		free(pos);
+		return (NULL);
+	}
+}
