d627e880d8d0dd44fd589eccb4f419937b52bfe7
Jose Alvarez <5135@holbertonschool.com>
2022-06-27 18:14:56 -0700

    Structures and typedef

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 			d->age = 0;
 		}
 printf("Name: %s\n", d->name);
-printf("Age: %f\n," d->age);
+printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
 	}
 }
