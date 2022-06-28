78f1b6781aa6b928e72f9380d64b2bc6c315337b
Jose Alvarez <5135@holbertonschool.com>
2022-06-28 06:24:14 -0700

    Structures and typedef

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -18,6 +18,27 @@ while (*s != '\0')
 return (count);
 }
 /**
+ * _strcpy - copy array
+ * @dest: destino de copia
+ * @src: original
+ * Return: 0;
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+if (!src || !dest)
+{
+	return (0);
+}
+while (src[count] != '\0')
+{
+dest[count] = src[count];
+count++;
+}
+dest[count] = '\0';
+return (dest);
+}
+/**
  * new_dog - dogchow
  * @name: name
  * @age: age
@@ -28,7 +49,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *pos;
 (void)age;
-int lname, lowner;
+int lname, lowner, i;
 
 pos = malloc(sizeof(dog_t));
 	if (pos == NULL)
@@ -41,13 +62,15 @@ lowner = _strlen(owner);
 pos->name = malloc(lname * sizeof(pos->name));
 	if (pos->name == NULL)
 	{
-		free(pos);
+		free(pos->name);
 		return (NULL);
 	}
 pos->owner = malloc(lowner * sizeof(pos->owner));
 	if (pos->owner == NULL)
 	{
-		free(pos);
+		free(pos->owner);
 		return(NULL);
 	}
+pos->age = age;
+	_strcpy(dog_t *pos, dog_t *new_dog)
 }
