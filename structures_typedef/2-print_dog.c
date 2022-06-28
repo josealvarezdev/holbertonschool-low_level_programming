763c8dd06d9728f03ec9c8cf5af48c4cec6e3d26
Jose Alvarez <5135@holbertonschool.com>
2022-06-27 18:09:10 -0700

    Structures and typedef

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - perritos
+ * @d: puntero delta
+*/ 
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+		d->owner = "(nil)";
+		}
+		if (d->age == NULL)
+		{
+			d->age = "(nil)";
+		}
+printf("Name: %s\n", d->name);
+printf("Age: %f\n," d->age);
+printf("Owner: %s\n", d->owner);
+	}
+}
