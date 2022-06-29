66297cf545324b00c491efd7e8dd92e250105cb6
Jose Alvarez <5135@holbertonschool.com>
2022-06-28 18:57:50 -0700

    Functions Pointers

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * array_iterator - array iterator douh
+ * @array: array
+ * @size: size
+ * @action: action douh
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+	if (size <= 0)
+	{
+		return(-1);
+	}
+	if(array != 0 && cmp != 0)
+	{
+		for (i = 0; size > i; i++)
+		{
+		if(cmp(array[i]));
+		}
+	}
+}
