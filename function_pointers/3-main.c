793412b37c52bdddbbb3c77e4cdb84ac94f0b1c3
Jose Alvarez <5135@holbertonschool.com>
2022-06-30 07:25:37 -0700

    Functions Pointers

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,7 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
-
+/**
+* main - check code
+* @argc: int
+* @argv: char
+* Return: Always 0
+*/
 int main(int argc, char *argv[])
 {
 	int result;
