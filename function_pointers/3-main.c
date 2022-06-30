cf6d4d07cfa1678f89d309d4f70e4333524af995
Jose Alvarez <5135@holbertonschool.com>
2022-06-30 07:19:14 -0700

    Functions Pointers

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,26 +4,28 @@
 
 int main(int argc, char *argv[])
 {
-int res;
-	if (argc != 4)
+	int result;
+
+	if (argc < 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[3]) == 0 && (argv [2]) % == 0)
+	if (((*argv[2] == '/') || (*argv[2] == '%')) && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if ((*(argv[2] == '/' && (argv[2] == '%')) && (atoi(argv[3])== 0)))
+	if ((*(get_op_func(argv[2]))) && (strlen(argv[2]) == 1))
 	{
-		printf("%d\n", res);
-		exit(100);
+		result = (*(get_op_func(argv[2])))(atoi(argv[1]), atoi(argv[3]));
+		printf("%d\n", result);
 	}
 	else
 	{
-	printf("Error\n");
-	exit(99);
+		printf("Error\n");
+		exit(99);
 	}
-return (0);
+
+	return (0);
 }
