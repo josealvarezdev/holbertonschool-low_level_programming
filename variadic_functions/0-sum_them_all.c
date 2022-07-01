23162eaae67354c90b5c10d8bc9ff284273149e4
Jose Alvarez <5135@holbertonschool.com>
2022-07-01 10:22:25 -0700

    Micha is love

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,12 +11,12 @@ int sum_them_all(const unsigned int n, ...)
 va_list ar;
 unsigned int sum, i;
 
-va_start (ar, n);
+va_start(ar, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ar, int);
 	}
-va_end (ar);
+va_end(ar);
 
 return (sum);
 }
