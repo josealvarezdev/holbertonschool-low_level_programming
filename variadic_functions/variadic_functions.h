895a905b625530fa11ff166a4bd607f4e8239858
Jose Alvarez <5135@holbertonschool.com>
2022-07-04 06:26:04 -0700

    Micha is love

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,7 +8,7 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-typedef struct picker
+typedef struct pick
 {
 	char let;
 	void (*f)(va_list);
