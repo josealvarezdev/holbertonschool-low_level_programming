1ac75ceb38efd73b88b39216abd441d3bfc35f8c
Jose Alvarez <5135@holbertonschool.com>
2022-07-12 10:41:21 -0700

    asd

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,19 +10,18 @@ size_t print_list(const list_t *h)
 int count = 0;
 const list_t *p = h;
 
-	for (; p; p = p->next)
-	{
-	if (p->str)
-	{
-		printf("[%u] %s", p->len, p->str);
-	}
-	else
-	{
-	printf("[0] (nil)");
-	}
-
-	count++;
-	putchar(10);
-	}
+for (; p; p = p->next)
+{
+if (p->str)
+{
+printf("[%u] %s", p->len, p->str);
+}
+else
+{
+printf("[0] (nil)");
+}
+count++;
+putchar(10);
+}
 return (count);
 }
