0b9921785d04404ce4d961e46fcbbb86271dc41b
Jose Alvarez <5135@holbertonschool.com>
2022-07-12 17:16:56 -0700

    as

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 #include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 
 list_t *add_node(list_t **head, const char *str)
 
+char *newlist;
 char *strcp;
 strcp = strdup(str);
 
-list_t *newlist= malloc(sizeof(list_t));
+list_t newlist= malloc(sizeof(list_t));
 
-if (new != NULL)
+if (newlist != NULL)
 {
 	newlist->str = strcp;
 	newlist->len = strlen(strcp);
