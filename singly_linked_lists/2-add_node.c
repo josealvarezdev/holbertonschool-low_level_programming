27126dc8698268487151ed23a93d19b295654a5e
Jose Alvarez <5135@holbertonschool.com>
2022-07-12 17:45:54 -0700

    s

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,12 +3,16 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* add_node - agrega nodo al inicio
+* @head: head
+* @str: string
+*/
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newlist;
 
-	newlist= malloc(sizeof(list_t));
+	newlist = malloc(sizeof(list_t));
 
 	if (!newlist)
 		return (NULL);
