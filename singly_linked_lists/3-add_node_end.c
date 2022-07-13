96b9293411cb51b130521f84e7bf05577dbe5e54
Jose Alvarez <5135@holbertonschool.com>
2022-07-13 10:23:58 -0700

    3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -28,7 +28,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = newlist;
 		return (*head);
 	}
-	while (p != NULL && P->next != NULL)
+	while (p != NULL && p->next != NULL)
 	{
 		p = p->next;
 	}
