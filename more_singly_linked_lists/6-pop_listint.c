0acb8184f0c462b31bb1202c918264d031024537
Jose Alvarez <5135@holbertonschool.com>
2022-07-14 09:27:09 -0700

    a

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes head node
+ * @head: header singly linked list
+ * Return: value
+ */
+int pop_listint(listint_t **head)
+{
+int k = 0;
+listint_t *p = *head
+
+if (*head == NULL)
+	return (0);
+
+	*head = p->next;
+	k = p->n;
+	free (p);
+	return (k);
+}
