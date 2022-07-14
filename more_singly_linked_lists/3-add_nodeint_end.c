a087fe2887855de6c1598047c314a61eb2a9d52c
Jose Alvarez <5135@holbertonschool.com>
2022-07-14 08:30:43 -0700

    a

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,8 +10,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 listint_t *newnode;
 listint_t *aux;
 
+aux = (*head);
 newnode = malloc(sizeof(listint_t));
-newnode = (*head);
 
 if (!newnode)
 	return (NULL);
