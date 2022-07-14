1e5d267994be332d7f451659eab5a831a541f63e
Jose Alvarez <5135@holbertonschool.com>
2022-07-14 07:58:02 -0700

    a

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *p;
+
+p = malloc(sizeof(listint_t));
+
+	if (!p)
+		return (NULL);
+	if (p != NULL)
+	{
+		p->n = n
+		p->next = (*head);
+		(*head) = p;
+	}
+return (*head);
+}
