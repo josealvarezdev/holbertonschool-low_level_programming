3716e0baede09970f0f1e2593d05415a89698d92
Jose Alvarez <5135@holbertonschool.com>
2022-07-14 08:20:52 -0700

    a

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 listint_t *newnode;
 listint_t *aux;
 
-newenode = malloc(sizeof(listint_t));
+newnode = malloc(sizeof(listint_t));
 newnode = (*head);
 
 if (!newnode)
