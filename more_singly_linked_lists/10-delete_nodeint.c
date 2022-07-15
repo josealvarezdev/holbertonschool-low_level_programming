159bdf0365d612e186994a1bd8bf561154c22ef5
Jose Alvarez <5135@holbertonschool.com>
2022-07-15 10:05:55 -0700

    a

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -12,10 +12,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 listint_t *tmp = *head;
 listint_t *aft;
 
-unsigned int idx = 1;
+unsigned int idx = 0;
 
 	if (!tmp)
-		return (-1)
+		return (-1);
 	if (index == 0)
 	{
 		*head = tmp->next;
