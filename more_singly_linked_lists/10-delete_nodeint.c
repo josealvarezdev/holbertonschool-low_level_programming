fc5ae34b5a53b4bdc2d3bfa61c6ce5c4691072e8
Jose Alvarez <5135@holbertonschool.com>
2022-07-15 10:00:23 -0700

    Ex10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - delete a new node at a given position
+ *
+ * @head: pointer to list_t
+ * @index:  is the index of the node
+ *
+ * Return: nth node of a listint_t linked list
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *tmp = *head;
+listint_t *aft;
+
+unsigned int idx = 1;
+
+	if (!tmp)
+		return (-1)
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	while (tmp && idx < index - 1)
+	{
+		tmp = tmp->next;
+		idx++;
+	}
+	if (!tmp || !(tmp->next))
+	{
+		return (-1);
+	}
+aft = tmp->next;
+tmp->next = aft->next;
+
+free(prev);
+return (1);
+}
