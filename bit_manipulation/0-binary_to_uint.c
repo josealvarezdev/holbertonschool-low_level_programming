94557747375806180b05c027685d5c44a8e96e20
Jose Alvarez <5135@holbertonschool.com>
2022-07-18 10:15:51 -0700

    hotfix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,34 @@
 #include "main.h"
-
+/**
+ * binary_to_uint - converts a binary number
+ *
+ * @b: string
+ *
+ * Return: converted number
+ */
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int res = 0;
 int i;
 
 if (!b)
+{
 	return (0);
-
+}
 	for (i = 0; b[i] != '\0'; i++)
-
+	{
 		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
+	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		res *=2;
 			if (b[i] == '1')
+			{
 				res +=1;
+			}
 	}
 return (res);
 }
