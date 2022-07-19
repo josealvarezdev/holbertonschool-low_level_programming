dfd640574af6d455b3067d1a12055c048e91f482
Jose Alvarez <5135@holbertonschool.com>
2022-07-19 11:06:43 -0700

    task1 finish

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,6 +1,9 @@
 #include "main.h"
-
 /**
+ * create_file - create a file
+ * @filename: file name
+ * @text_content: li text content
+ * Return: 1 if sucess or -1 if an error ocurred
 */
 int create_file(const char *filename, char *text_content)
 {
