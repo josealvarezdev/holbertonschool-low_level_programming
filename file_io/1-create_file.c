879af71bb507ae5c3c2ec1c6c758c3d60ea80c3e
Jose Alvarez <5135@holbertonschool.com>
2022-07-19 10:52:51 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+/**
+*/
 int create_file(const char *filename, char *text_content)
 {
 int cr, wr;
@@ -7,7 +9,7 @@ int cr, wr;
 if (!filename)
 	return (-1);
 
-cr = open(filename, O_RDWR | O_CREAT, 0600);
+cr = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 if (cr == -1)
 	return (-1);
