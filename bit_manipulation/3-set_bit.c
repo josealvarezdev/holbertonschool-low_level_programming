ec7bb7a23deb618008ce5db0af353e7f06a5f82c
Jose Alvarez <5135@holbertonschool.com>
2022-07-19 07:46:29 -0700

    task3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -2,17 +2,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned int i;
-
 	if (index > 63)
 		return (-1);
 
-	for (i = 0; n[i] != '0'; i++)
-	{
-		if (i == index)
-		{
-			*n ^= (1 << index);
-		}
-	}
-return (1);
+	*n |= 1 << index;
+	return (1);
 }
