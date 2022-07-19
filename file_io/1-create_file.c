583c81e67de113f5d173554c0f3499c4472efb07
Jose Alvarez <5135@holbertonschool.com>
2022-07-19 10:48:26 -0700

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+int cr, wr;
+
+if (!filename)
+	return (-1);
+
+cr = open(filename, O_RDWR | O_CREAT, 0600);
+
+if (cr == -1)
+	return (-1);
+
+wr = write(cr, text_content, strlen(text_content));
+if (wr == -1)
+{
+	close (cr);
+	return (-1);
+}
+close (cr);
+return (1);
+}
