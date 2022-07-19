bbf1960227a09979e96ed451de9ff0ab2e15d828
Jose Alvarez <5135@holbertonschool.com>
2022-07-19 10:54:43 -0700

    a

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,22 +4,25 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int cr, wr;
+	int cr, wr;
 
-if (!filename)
-	return (-1);
+	if (!filename)
+		return (-1);
 
-cr = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (!text_content)
+		return (-1);
 
-if (cr == -1)
-	return (-1);
+	cr = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-wr = write(cr, text_content, strlen(text_content));
-if (wr == -1)
-{
+	if (cr == -1)
+		return (-1);
+
+	wr = write(cr, text_content, strlen(text_content));
+	if (wr == -1)
+	{
+		close (cr);
+		return (-1);
+	}
 	close (cr);
-	return (-1);
-}
-close (cr);
-return (1);
+	return (1);
 }
