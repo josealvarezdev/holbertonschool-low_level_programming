18afdaf0c4e57096d57f13f5951d3cd23139f3ec
Jose Alvarez <5135@holbertonschool.com>
2022-07-19 11:03:21 -0700

    taks1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,18 +9,17 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
-
 	cr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (cr == -1)
 		return (-1);
 
+	if (!text_content)
+		return (1);
+
 	wr = write(cr, text_content, strlen(text_content));
 	if (wr == -1)
 	{
-		close (cr);
 		return (-1);
 	}
 	close (cr);
