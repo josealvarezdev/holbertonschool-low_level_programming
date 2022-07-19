7b946b06a7cff48d8441a56b760066296f76a425
Jose Alvarez <5135@holbertonschool.com>
2022-07-19 09:18:57 -0700

    add main and 0 task

---
+++ bit_manipulation/file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * read_textfile - read and print with POSIX
+ * @filename: pointer to string
+ * @letter: number of letters of string
+ * Return: 0 if an error ocurred
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char buffer[letters];
+return (0)
+}
