dfeb505855ae60f6a2e65c8abf93b2bfcd2c5622
Jose Alvarez <5135@holbertonschool.com>
2022-07-20 16:15:00 -0700

    Q rompimo

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,40 +21,38 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = argv[1];
-	file_to = argv[2];
 	fdir_from = open(file_from, O_RDONLY, 0664);
 
-	if (file_from < 0)
+	if (argv[1] < 0)
 	{
 		dprintf(STERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
 	fdir_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to < 1)
+	if (argv[2] < 1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	rd = read(fdir_from, buffer, 1024);
 	if (fdir_to < 0 || (write(fdir_to, buffer, rd) != rd))
 	{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	exit(99);
 	}
 	if (rd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(file_from) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdir_from);
 		exit(100);
 	}
 	if (close(file_to) < 0);
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdir_to);
 		exit(100);
 	}
 	return (0);
