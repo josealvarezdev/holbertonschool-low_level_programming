65f5258c94d265d21f8d8cbf49dc654e0dd47c34
Jose Alvarez <5135@holbertonschool.com>
2022-07-20 16:32:44 -0700

    a

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fdir_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while (buffer == 1024)
+	while ((r = read(fdir_from, buffer, 1024)) > 0)
 	{
 		rd = read(fdir_from, buffer, 1024);
 		if (fdir_to < 0 || (write(fdir_to, buffer, rd) != rd))
