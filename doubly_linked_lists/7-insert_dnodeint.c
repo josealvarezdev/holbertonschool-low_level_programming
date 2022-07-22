de53cd700fccb97a76e550825f564c2c159014a3
Jose Alvarez <5135@holbertonschool.com>
2022-07-22 09:16:10 -0700

    a

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,9 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode = malloc(sizeof(dlistint_t));
 		if(newnode == NULL)
 			return (NULL);
-		newnode->n = n;
-		newnode->prev = tmp;
-		newnode->next = tmp->next;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
@@ -28,5 +25,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
+	newnode->n = n;
+	newnode->prev = tmp;
+	newnode->next = tmp->next;
+	tmp->next->prev = newnode;
+	tmp->next = newnode;
 	return (newnode);
 }
