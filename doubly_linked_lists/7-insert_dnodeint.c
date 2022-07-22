b1656de896909a33328187ec2fd4944e27075695
Jose Alvarez <5135@holbertonschool.com>
2022-07-22 09:06:26 -0700

    task7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+* insert_dnodeint_at_index - insert a new node in a given position
+* @h: head
+* @idx: unsigned int
+* @n: int
+* Return: *head
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newnode, *tmp = *h;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+		return (NULL);
+	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->prev = tmp;
+	newnode->next = tmp->next;
+	tmp->next->prev = newnode;
+	tmp->next = newnode;
+
+	return (newnode);
+}
