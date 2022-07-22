331dee571d0b0023422ac97c928b71e55d839b7f
Jose Alvarez <5135@holbertonschool.com>
2022-07-22 09:17:05 -0700

    asd

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newnode, *tmp = *h;
 
 	newnode = malloc(sizeof(dlistint_t));
-		if(newnode == NULL)
+		if (newnode == NULL)
 			return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
