f98bab36ce65805810ed32db7763e322411a72e6
Jose Alvarez <5135@holbertonschool.com>
2022-07-21 18:11:42 -0700

    a

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	sig = bef->next;
 	sig->prev = new;
 	bef->next = new;
-	new->prev = aux;
+	new->prev = bef;
 	new->next = sig;
 	return (new);
 }
