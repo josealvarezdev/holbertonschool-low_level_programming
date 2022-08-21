a22685e0a136345682f070dc97a9fe617e72138e
Bester <besster.gg@gmail.com>
2022-08-21 18:42:24 -0300

    task4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,24 +1,22 @@
 #include "hash_tables.h"
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i;
 	hash_node_t *p;
 
-    if (!ht)
-        return (NULL);
+	if (!ht)
+		return (NULL);
 	for (i = 0; ht && i < ht->size; i++)
 	{
 		p = ht->array[i];
 		while (p)
 		{
-            if (strcmp(p->key, strdup(key)) == 0)
-            {
-                return (p->value);
-            }
-			p = p-> next;
+			if (strcmp(p->key, strdup(key)) == 0)
+			{
+				return (p->value);
+		       	}
+			p  = p-> next;
 		}
-		printf("\n");
 	}
-    return (NULL);
+	return (NULL);
 }
