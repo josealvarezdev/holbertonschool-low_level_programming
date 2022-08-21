4e723b08e177bc5c8bcda1277204184a498c05ef
Jose Alvarez <5135@holbertonschool.com>
2022-08-21 08:08:49 -0700

    2 key index

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hast_tables.h"
+/**
+ * key_index - function that gives you the index of a key
+ * @size: size of array
+ * @key: is key
+ * Return: the index which is the key % size of array
+ * Previusly djb2 multiply each character by 33 2^5 + 1
+ * And return the key word with a number which
+ * represent less colissions
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+return (hash_djb2(key) % size);
+}
