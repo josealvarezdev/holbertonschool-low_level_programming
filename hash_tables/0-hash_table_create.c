d36beb0ee62638d9d114a3382d5ed3907a5af24c
Jose Alvarez <5135@holbertonschool.com>
2022-08-21 05:37:17 -0700

    task0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *ht = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if(!ht)
+		return(NULL);
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_table_t *));
+	if(!ht-array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	while(i < size)
+		ht->array[i] = NULL;
+	return (ht);
+}
