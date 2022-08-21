e26082585980d3fdd6e5c26126bb8f823edbf32c
Bester <besster.gg@gmail.com>
2022-08-21 20:31:53 -0300

    task 6 try 1

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print hash table
+ * @ht: table
+ * Return: if ht dosent exist dont print
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p;
+
+	if (!ht)
+		return;
+	for (i = 0; ht && i < ht->size; i++)
+	{
+		p = ht->array[i];
+		while (p)
+		{
+			if (p->key != NULL && p->value != NULL)
+                free(p->key);
+                free(p->value);
+                free(p);
+		}
+	}
+	free(ht->array);
+    free(ht);
+}
