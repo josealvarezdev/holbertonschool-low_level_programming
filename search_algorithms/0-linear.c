94341a39cb8d9872951d9bc0cc41bbb2eb5357b6
Jose Alvarez <5135@holbertonschool.com>
2022-08-29 13:53:56 -0700

    task 0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ * linear_search - linear search algorithm
+ * @array: array
+ * @size: size of array
+ * @value: value to search
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	if (!array)
+		return (-1);
+	for (; i < (int) size; i++)
+	{
+		printf("Value checked array [%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			break;
+	}
+	if (i >= (int) size)
+		return (-1);
+	else
+		return (i);
+}
