a5edc72b55c04178f3c33929233fcf8342659a2c
Jose Alvarez <5135@holbertonschool.com>
2022-08-30 14:26:12 -0700

    task

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t start = 0, end = size - 1, mid = 0, i = 0;
+	
+	if (!array)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (; i < size; i++)
+			printf ("%d%s", array[i], i != size - 1 ? ", " : "");
+	printf("\n");
+	while (start <= end)
+	{
+		mid = (int)start + (end - start) / 2;
+		printf("Searching in array: ");
+		for (i = 0; i < mid; i++)
+			printf ("%d%s", array[i], i != (mid - 1) ? ", " : "");
+		printf("\n");
+		if (array[mid]  == value)
+			break;
+		else if (array[mid] < value)
+			start = mid + 1;
+		else
+			end = mid - 1;
+		}
+	if (array[i] == value)
+		return (mid);
+	else
+		return (-1);
+}
